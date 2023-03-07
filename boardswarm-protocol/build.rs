a27b8d813a8d571b50c77890cf5b1c50296d43e6
Sjoerd Simons <sjoerd@collabora.com>
2023-03-07 21:57:57 +0100

    Fix clippy warning (clippy::needless_borrow)

--- boardswarm-protocol/build.rs
+++ boardswarm-protocol/build.rs
@@ -2,7 +2,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut config = prost_build::Config::new();
     config
         .protoc_arg("--experimental_allow_proto3_optional")
-        .bytes(&["."])
+        .bytes(["."])
         .extern_path(".google.protobuf.Struct", "Parameters");
 
     let empty: &[&str] = &[];
