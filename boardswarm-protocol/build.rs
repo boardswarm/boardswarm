2a6396a065b48c39f6590cb53122abafec5c242f
Sjoerd Simons <sjoerd@collabora.com>
2023-03-07 20:12:11 +0100

    Fix build on Debian bullseye
    The protobuf compiler in Debian Bullseye doesn't yet support
    optional fields by default; So enable it explicitly which works for both
    old and new protoc
    
    Signed-off-by: Sjoerd Simons <sjoerd@collabora.com>

--- boardswarm-protocol/build.rs
+++ boardswarm-protocol/build.rs
@@ -1,6 +1,7 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut config = prost_build::Config::new();
     config
+        .protoc_arg("--experimental_allow_proto3_optional")
         .bytes(&["."])
         .extern_path(".google.protobuf.Struct", "Parameters");
 
