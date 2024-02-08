1666792a2c9915df9cbfa53498d43a65ceeb0fd7
Christopher Obbard <chris.obbard@collabora.com>
2024-02-08 09:57:23 +0000

    Add context to configuration file loading error
    Currently if the configuration file is not found, boardswarm provides a
    generic error:
    
        $ boardswarm null
        Error: No such file or directory (os error 2)
    
    This can be unhelpful when determining what caused the error. Add some
    context explaining that the configuration file failed to load:
    
        $ boardswarm null
        Error: Failed to load configuration file null
    
        Caused by:
            No such file or directory (os error 2)
    
    Signed-off-by: Christopher Obbard <chris.obbard@collabora.com>

--- boardswarm/src/main.rs
+++ boardswarm/src/main.rs
@@ -1,4 +1,4 @@
-use anyhow::bail;
+use anyhow::{bail, Context};
 use boardswarm_protocol::item_event::Event;
 use boardswarm_protocol::{
     console_input_request, volume_io_reply, volume_io_request, ConsoleConfigureRequest,
@@ -933,7 +933,11 @@ async fn main() -> anyhow::Result<()> {
     tracing_subscriber::fmt::init();
 
     let opts = Opts::parse();
-    let config = config::Config::from_file(&opts.config)?;
+    let config = config::Config::from_file(&opts.config).context(format!(
+        "Failed to load configuration file {}",
+        opts.config.display()
+    ))?;
+
     let listen_config = config
         .server
         .listen
