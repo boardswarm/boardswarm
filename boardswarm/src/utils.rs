6943af944fde85eaacb26100398f7a9624e64a47
Sjoerd Simons <sjoerd@collabora.com>
2024-10-30 21:03:40 +0100

    boardswarm: Port rockusb support to nusb
    Move from the rockusb libusb backend to the nusb backend for native usb
    support.

--- boardswarm/src/utils.rs
+++ boardswarm/src/utils.rs
@@ -1,5 +1,16 @@
 use tracing::warn;
 
+pub fn nusb_info_from_bus_dev(bus: u8, dev: u8) -> Option<nusb::DeviceInfo> {
+    let mut devices = match nusb::list_devices() {
+        Ok(d) => d,
+        Err(e) => {
+            warn!("Failed to get usb device list: {e}");
+            return None;
+        }
+    };
+    devices.find(|d| d.bus_number() == bus && d.device_address() == dev)
+}
+
 pub fn usb_device_from_bus_dev(bus: u8, dev: u8) -> Option<rusb::Device<rusb::GlobalContext>> {
     let devices = match rusb::DeviceList::new() {
         Ok(devices) => devices,
