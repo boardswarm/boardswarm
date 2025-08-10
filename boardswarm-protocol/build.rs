3242ed1b856326f039cf35d14bc1c4f5334bab4c
Sjoerd Simons <sjoerd@collabora.com>
2025-08-10 08:28:52 +0200

    chore: Update to tonic 0.14

--- boardswarm-protocol/build.rs
+++ boardswarm-protocol/build.rs
@@ -1,12 +1,12 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut config = prost_build::Config::new();
+    let mut config = tonic_prost_build::Config::new();
     config
         .protoc_arg("--experimental_allow_proto3_optional")
         .bytes(["."])
         .extern_path(".google.protobuf.Struct", "Parameters");
 
     let empty: &[&str] = &[];
-    tonic_build::configure().compile_protos_with_config(
+    tonic_prost_build::configure().compile_with_config(
         config,
         &["proto/boardswarm.proto"],
         empty,
